49094f0d0f04fa89882bcfe5b996a66ebd69ebd4
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-05 04:40:11 +0100

    Create App.js

---
+++ src/components/ContactForm/App.js
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>Request your original diamond right now!</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
