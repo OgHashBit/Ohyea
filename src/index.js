43759718fabe4745b9489525fd77fb1a10c8eafe
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-05 04:25:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import RefractionMaterial from "./refraction-material"
 import diamondUrl from "./assets/diamond.glb"
 import textureUrl from "./assets/233.jpg"
 import "./styles.css"
+import './index.css';
+import App from './App';
 
 function Background() {
   const texture = useTexture(textureUrl)
